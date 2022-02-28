6304371d1629f90d7cdf057de73459286a4b678f
zuluzza <zuluzza@gmail.com>
2022-02-28 20:21:50 +0200

    Go through predictions by league

--- main.py
+++ main.py
@@ -5,22 +5,29 @@ def predictionString2Odds(str):
 	return 1.0 / (float(str.strip("%")) / 100)
 
 URL = 'https://kickoff.ai/matches'
-matches_page = requests.get(URL)
 
-parsed_matches = BeautifulSoup(matches_page.content, 'html.parser')
+def predict_group(group_id):
+	matches_page = requests.get(URL + "/" + group_id)
 
-#TODO there's show more button on the web page. This does not find the matches hidden behind it
-predictions = parsed_matches.find_all("div", {"class": "prediction prediction-fixture"})
+	parsed_matches = BeautifulSoup(matches_page.content, 'html.parser')
 
-for pred in predictions:
-	home_team = pred.find("div", {"class": "team-home"}).text
-	away_team = pred.find("div", {"class": "team-away"}).text
+	#TODO there's show more button on the web page. This does not find the matches hidden behind it
+	predictions = parsed_matches.find_all("div", {"class": "prediction prediction-fixture"})
 
-	home_win = pred.find("span", {"class": "prediction-win-home"}).text
-	home_win = predictionString2Odds(home_win)
-	tie = pred.find("span", {"class": "prediction-draw"}).text
-	tie = predictionString2Odds(tie)
-	away_win = pred.find("span", {"class": "prediction-win-away"}).text
-	away_win = predictionString2Odds(away_win)
+	for pred in predictions:
+		home_team = pred.find("div", {"class": "team-home"}).text
+		away_team = pred.find("div", {"class": "team-away"}).text
 
-	print("{} vs {}Predicted odds: home {} - tie {} - away {}".format(home_team, away_team, home_win, tie, away_win))
+		home_win = pred.find("span", {"class": "prediction-win-home"}).text
+		home_win = predictionString2Odds(home_win)
+		tie = pred.find("span", {"class": "prediction-draw"}).text
+		tie = predictionString2Odds(tie)
+		away_win = pred.find("span", {"class": "prediction-win-away"}).text
+		away_win = predictionString2Odds(away_win)
+
+		print("{} vs {}: predicted odds: home {} - tie {} - away {}".format(home_team.strip(), away_team.strip(), home_win, tie, away_win))
+
+groups = ["29", "31", "35", "33", "27"]
+
+for id in groups:
+	predict_group(id)
